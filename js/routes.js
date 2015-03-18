9353f0c1b66d22c583143b163221d28d6d01b131
grizzli <r.abd@mail.com>
2015-03-18 23:06:26 +0200

    Commit

---
+++ js/routes.js
@@ -0,0 +1,12 @@
+AngularApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/books', {
+            templateUrl: 'templates/pages/books/table.html'
+        })
+        .when('/authors', {
+            templateUrl: 'templates/pages/authors/table.html'
+        })
+        .otherwise({
+            redirectTo: '/books'
+        });
+}]);
--- js/script.js
+++ js/script.js
@@ -1,239 +1,224 @@
-(function () {
-    var app = angular.module('newModule', ['ngRoute']);
-        app.config(['$routeProvider', function ($routeProvider) {
-            $routeProvider
-                .when('/books', {
-                    templateUrl: 'templates/pages/books/table.html'
-                })
-                .when('/authors', {
-                    templateUrl: 'templates/pages/authors/table.html'
-                })
-                .otherwise({
-                    redirectTo: '/books'
-                });
-        }])
-        .controller('appController', ['$scope', function ($scope) {
-            $scope.activeTab = 1;
-            $scope.setTab = function (arg) {
-                $scope.activeTab = arg;
-            };
+var AngularApp = angular.module('newModule', ['ngRoute'])
+    .controller('appController', ['$scope', function ($scope) {
+        $scope.activeTab = 1;
+        $scope.setTab = function (arg) {
+            $scope.activeTab = arg;
+        };
 
-            $scope.displayDialogContent = {
-                displayAddAuthor: false,
-                displayAddBook: false,
-                displayConfirmationDialog: false
-            };
-            $scope.displayContent = function (content) {
-                $.each($scope.displayDialogContent, function (key, value) {
-                    $scope.displayDialogContent[key] = false;
-                });
-                $scope.displayDialogContent[content] = true;
-                return false;
-            };
-            $scope.returnCurrentDialogTitle = function () {
-                var result;
-                $.each($scope.displayDialogContent, function (key, value) {
-                    if ($scope.displayDialogContent[key]) {
-                        switch (key) {
-                            case 'displayAddAuthor':
-                                result = 'Add author';
-                                break;
-                            case 'displayAddBook':
-                                result = 'Add book';
-                                break;
-                            case 'displayConfirmationDialog':
-                                result = 'Confirm your action';
-                                break;
-                        }
+        $scope.displayDialogContent = {
+            displayAddAuthor: false,
+            displayAddBook: false,
+            displayConfirmationDialog: false
+        };
+        $scope.displayContent = function (content) {
+            $.each($scope.displayDialogContent, function (key, value) {
+                $scope.displayDialogContent[key] = false;
+            });
+            $scope.displayDialogContent[content] = true;
+            return false;
+        };
+        $scope.returnCurrentDialogTitle = function () {
+            var result;
+            $.each($scope.displayDialogContent, function (key, value) {
+                if ($scope.displayDialogContent[key]) {
+                    switch (key) {
+                        case 'displayAddAuthor':
+                            result = 'Add author';
+                            break;
+                        case 'displayAddBook':
+                            result = 'Add book';
+                            break;
+                        case 'displayConfirmationDialog':
+                            result = 'Confirm your action';
+                            break;
                     }
-                });
-                return result;
-            };
-        }])
-
-        .controller('booksController', ['$http', '$scope', function ($http, $scope) {
-            $scope.getCollection = function (collection) {
-                if (localStorage[collection]) {
-                    return JSON.parse(localStorage[collection]);
                 }
-            };
-            $scope.saveCollection = function (collection, data) {
-                localStorage[collection] = JSON.stringify(data);
-            };
+            });
+            return result;
+        };
+    }])
 
-            if (!$scope.getCollection('myBooksCollection')) {
-                $http.get('data/data.books.json').success(function (data) {
-                    $scope.saveCollection('myBooksCollection', data);
-                });
+    .controller('booksController', ['$http', '$scope', function ($http, $scope) {
+        $scope.getCollection = function (collection) {
+            if (localStorage[collection]) {
+                return JSON.parse(localStorage[collection]);
             }
-            if (!$scope.getCollection('myAuthorsCollection')) {
-                $http.get('data/data.authors.json').success(function (data) {
-                    $scope.saveCollection('myAuthorsCollection', data);
-                });
+        };
+        $scope.saveCollection = function (collection, data) {
+            localStorage[collection] = JSON.stringify(data);
+        };
+
+        if (!$scope.getCollection('myBooksCollection')) {
+            $http.get('data/data.books.json').success(function (data) {
+                $scope.saveCollection('myBooksCollection', data);
+            });
+        }
+        if (!$scope.getCollection('myAuthorsCollection')) {
+            $http.get('data/data.authors.json').success(function (data) {
+                $scope.saveCollection('myAuthorsCollection', data);
+            });
+        }
+        $scope.itemToRemove = {
+            idToRemove: '',
+            typeToRemove: ''
+        };
+
+        $scope.removeItem = function (id, type) {
+            if (type == 'book') {
+                for (var i = 0; i < $scope.books.length; i++) {
+                    if ($scope.books[i].id == id) {
+                        $scope.books.splice(i, 1);
+                        $scope.saveCollection('myBooksCollection', $scope.books);
+                        return false;
+                    }
+                }
             }
-            $scope.itemToRemove = {
-                idToRemove: '',
-                typeToRemove: ''
-            };
-
-            $scope.removeItem = function (id, type) {
-                if (type == 'book') {
-                    for (var i = 0; i < $scope.books.length; i++) {
-                        if ($scope.books[i].id == id) {
-                            $scope.books.splice(i, 1);
-                            $scope.saveCollection('myBooksCollection', $scope.books);
-                            return false;
-                        }
+            else {
+                for (var i = 0; i < $scope.authors.length; i++) {
+                    if ($scope.authors[i].id == id) {
+                        $scope.authors.splice(i, 1);
+                        $scope.saveCollection('myAuthorsCollection', $scope.authors);
+                        return false;
                     }
                 }
-                else {
-                    for (var i = 0; i < $scope.authors.length; i++) {
-                        if ($scope.authors[i].id == id) {
-                            $scope.authors.splice(i, 1);
-                            $scope.saveCollection('myAuthorsCollection', $scope.authors);
-                            return false;
-                        }
-                    }
-                }
-                $scope.idToRemove = '';
-                $scope.typeToRemove = '';
-            };
+            }
+            $scope.idToRemove = '';
+            $scope.typeToRemove = '';
+        };
 
-            $scope.book = {};
-            $scope.books = $scope.getCollection('myBooksCollection') || [];
+        $scope.book = {};
+        $scope.books = $scope.getCollection('myBooksCollection') || [];
+        $scope.author = {};
+        $scope.authors = $scope.getCollection('myAuthorsCollection') || [];
+        $scope.requiredAuthors = [];
+
+        $scope.addRequiredAuthor = function (arg) {
+            for (var i = 0; i < $scope.authors.length; i++) {
+                if ($scope.authors[i].name == arg) {
+                    $scope.authors[i].invisible = true;
+                }
+            }
+            $scope.requiredAuthors.push(arg);
+        };
+
+        $scope.removeRequiredAuthor = function (arg) {
+            for (var i = 0; i <= $scope.requiredAuthors.length; i++) {
+                if ($scope.requiredAuthors[i] == arg) {
+                    $scope.requiredAuthors.splice(i, 1);
+                }
+            }
+            for (var j = 0; j < $scope.authors.length; j++) {
+                if ($scope.authors[j].name == arg) {
+                    $scope.authors[j].invisible = false;
+                }
+            }
+        };
+        $scope.addAuthor = function () {
+            $scope.authors.push($scope.author);
+            $scope.saveCollection('myAuthorsCollection', $scope.authors);
             $scope.author = {};
             $scope.authors = $scope.getCollection('myAuthorsCollection') || [];
+
+        };
+
+        $scope.addBook = function () {
+            $scope.book.author = $scope.requiredAuthors;
+            $scope.books.push($scope.book);
+            $scope.saveCollection('myBooksCollection', $scope.books);
             $scope.requiredAuthors = [];
+            $scope.book = {};
+            $scope.books = $scope.getCollection('myBooksCollection') || [];
+        };
 
-            $scope.addRequiredAuthor = function (arg) {
-                for (var i = 0; i < $scope.authors.length; i++) {
-                    if ($scope.authors[i].name == arg) {
-                        $scope.authors[i].invisible = true;
-                    }
+        $scope.getAuthorId = function (arg) {
+            for (var i = 0; i < $scope.authors.length; i++) {
+                if ($scope.authors[i].name == arg) {
+                    return $scope.authors[i].id;
                 }
-                $scope.requiredAuthors.push(arg);
-            };
-
-            $scope.removeRequiredAuthor = function (arg) {
-                for (var i = 0; i <= $scope.requiredAuthors.length; i++) {
-                    if ($scope.requiredAuthors[i] == arg) {
-                        $scope.requiredAuthors.splice(i, 1);
-                    }
-                }
-                for (var j = 0; j < $scope.authors.length; j++) {
-                    if ($scope.authors[j].name == arg) {
-                        $scope.authors[j].invisible = false;
-                    }
-                }
-            };
-            $scope.addAuthor = function () {
-                $scope.authors.push($scope.author);
-                $scope.saveCollection('myAuthorsCollection', $scope.authors);
-                $scope.author = {};
-                $scope.authors = $scope.getCollection('myAuthorsCollection') || [];
-
-            };
-
-            $scope.addBook = function () {
-                $scope.book.author = $scope.requiredAuthors;
-                $scope.books.push($scope.book);
-                $scope.saveCollection('myBooksCollection', $scope.books);
-                $scope.requiredAuthors = [];
-                $scope.book = {};
-                $scope.books = $scope.getCollection('myBooksCollection') || [];
-            };
-
-            $scope.getAuthorId = function (arg) {
-                for (var i = 0; i < $scope.authors.length; i++) {
-                    if ($scope.authors[i].name == arg) {
-                        return $scope.authors[i].id;
-                    }
-                }
-            };
-
-            $scope.restoreAuthors = function (arg) {
-                for (var i = 0; i < $scope.authors.length; i++) {
-                    $scope.authors[i].invisible = false;
-                }
-                $scope.cleanModel(arg);
-            };
-
-            $scope.editEntry = function () {
-
-            };
-
-            $scope.cleanModel = function (arg) {
-                if (typeof arg == "object") {
-                    arg = {};
-                }
-                else {
-                    $scope['arg'] = [];
-                }
-            };
-        }])
-
-    /**
-     * Directives
-     * */
-
-        .directive('appControls', function () {
-            return {
-                restrict: 'E',
-                scope: false,
-                templateUrl: 'templates/app-controls.html'
             }
-        })
+        };
 
-        .directive('dialogWrapper', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/dialog.html'
+        $scope.restoreAuthors = function (arg) {
+            for (var i = 0; i < $scope.authors.length; i++) {
+                $scope.authors[i].invisible = false;
             }
-        })
+            $scope.cleanModel(arg);
+        };
 
-        .directive('confirmation', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/confirmation.html'
+        $scope.editEntry = function () {
+
+        };
+
+        $scope.cleanModel = function (arg) {
+            if (typeof arg == "object") {
+                arg = {};
             }
-        })
-
-        .directive('confirmationFooter', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/confirmation-footer.html'
+            else {
+                $scope['arg'] = [];
             }
-        })
+        };
+    }])
 
-        .directive('addBook', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/add-book.html'
-            }
-        })
+/**
+ * Directives
+ * */
 
-        .directive('addAuthor', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/add-author.html'
-            }
-        })
+    .directive('appControls', function () {
+        return {
+            restrict: 'E',
+            scope: false,
+            templateUrl: 'templates/app-controls.html'
+        }
+    })
 
-        .directive('addBookFooter', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/add-book-footer.html'
-            }
-        })
+    .directive('dialogWrapper', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/dialog.html'
+        }
+    })
 
-        .directive('addAuthorFooter', function () {
-            return {
-                restrict: 'E',
-                templateUrl: 'templates/add-author-footer.html'
-            }
-        })
+    .directive('confirmation', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/confirmation.html'
+        }
+    })
 
-})();
+    .directive('confirmationFooter', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/confirmation-footer.html'
+        }
+    })
+
+    .directive('addBook', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/add-book.html'
+        }
+    })
+
+    .directive('addAuthor', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/add-author.html'
+        }
+    })
+
+    .directive('addBookFooter', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/add-book-footer.html'
+        }
+    })
+
+    .directive('addAuthorFooter', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/add-author-footer.html'
+        }
+    });
 //todo
 //валидация + валидация ид книги
 //редактирование
