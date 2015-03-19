4136f01b2dc1e4ea4c223f0283fba1a5f613f80d
grizzli <r.abd@mail.com>
2015-03-19 22:42:09 +0200

    Commit

--- js/controllers/booksCtrl.js
+++ js/controllers/booksCtrl.js
@@ -36,21 +36,18 @@ AngularApp.controller('booksController', ['$http', '$scope', 'dataService', func
     };
 
     if (!$dataService.getCollection('myBooksCollection')) {
-        $http.get('data/data.books.json').then(function (data) {
+        $http.get('data/data.books.json').success(function (data) {
             $dataService.setCollection('myBooksCollection', data);
         });
     }
     if (!$dataService.getCollection('myAuthorsCollection')) {
-        $http.get('data/data.authors.json').then(function (data) {
+        $http.get('data/data.authors.json').success(function (data) {
             $dataService.setCollection('myAuthorsCollection', data);
         });
     }
 
-    //setInterval(function(){
-        $scope.books = $dataService.getCollection('myBooksCollection') || [];
-        $scope.authors = $dataService.getCollection('myAuthorsCollection') || [];
-
-    //}, 200);
+    $scope.books = $dataService.getCollection('myBooksCollection') || [];
+    $scope.authors = $dataService.getCollection('myAuthorsCollection') || [];
 
     $scope.itemToRemove = {
         idToRemove: '',
