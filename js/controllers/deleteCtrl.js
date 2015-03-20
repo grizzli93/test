1a326635281d3ba907819e7a4897b1cf19facf55
Rustam Abduvaliiev <rustam.abduvaliiev@kruschecompany.com>
2015-03-20 19:03:27 +0200

    First working app

--- js/controllers/deleteCtrl.js
+++ js/controllers/deleteCtrl.js
@@ -1,13 +1,12 @@
-AngularApp.controller('deleteController', ['$scope','dataBus','dataService', function($scope, $dataBus, $dataService) {
-
+AngularApp.controller('deleteController', ['$scope', 'dataBus', 'dataService', function($scope, $dataBus, $dataService) {
+    $scope.test = '123';
+    $scope.lol = '452';
     $scope.removeItem = function () {
-        $scope.test = '123';
-        $scope.lol = '452';
-
 //        debugger;
         if ($dataBus.removeItems.dataToRemove  == 'book') {
             for (var i = 0; i < $dataBus.myBooks.length; i++) {
                 if ($dataBus.myBooks[i].id == $dataBus.removeItems.itemToRemove) {
+                    $scope.test = $dataBus.myBooks[i].name;
                     $dataBus.myBooks.splice(i, 1);
                     $dataService.setCollection('myBooksCollection', $dataBus.myBooks);
                     return false;
